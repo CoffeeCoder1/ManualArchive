b8bf4f520bf949f5012a38426eabbea01609fe02
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-05-18 11:19:03 -0400

    Update index.php

--- V1/index.php
+++ V1/index.php
@@ -1,3 +1,12 @@
-<?
-echo test;
-?>
+<doctype HTML>
+<htnml>
+  <head>
+    <title>Manual Archive</title>
+  </head>
+  <body>
+    This website is currently under construction. Please check back later.
+    <?
+    echo test;
+    ?>
+  </body>
+</html>
